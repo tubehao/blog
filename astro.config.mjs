c443009f6584b9af982f2d290b4d467466b7867f
tubehao <guanhao0412@163.com>
2024-10-24 16:32:42 +0800

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://tubehao.github.io/",
+  base: "/blog",
   trailingSlash: "always",
   integrations: [
     tailwind(),
